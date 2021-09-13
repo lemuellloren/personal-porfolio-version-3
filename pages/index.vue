53f25974a683f0830bb0b21b63dff14728ba6bd3
lemuellloren <lempakz@yahoo.com>
2021-09-13 08:59:22 +0800

    add variables ans install sass

--- pages/index.vue
+++ pages/index.vue
@@ -7,3 +7,15 @@
 <script>
 export default {}
 </script>
+
+<style lang="scss">
+//* Variables 
+$primary-color: #7D5FFF;
+$secondary-color: #1A202C;
+$tertiary-color: #F7F7F7;
+$dark-bg: #1D2430;
+$body-text: 18px;
+$section-title: 30px;
+$project-title: 24px;
+
+</style>
