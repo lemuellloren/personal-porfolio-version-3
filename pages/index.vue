f4c7c0b68ddbee5e037547e1e1613c15c35137fa
lemuellloren <lempakz@yahoo.com>
2021-09-13 09:08:25 +0800

    create head component

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,21 @@
 <template>
- <div class="container">
-   <h1>Hello World!</h1>
- </div>
+  <div class="container">
+    <TheHeader></TheHeader>
+  </div>
 </template>
 
 <script>
-export default {}
+  export default {}
 </script>
 
 <style lang="scss">
-//* Variables 
-$primary-color: #7D5FFF;
-$secondary-color: #1A202C;
-$tertiary-color: #F7F7F7;
-$dark-bg: #1D2430;
-$body-text: 18px;
-$section-title: 30px;
-$project-title: 24px;
+  //* Variables 
+  $primary-color: #7D5FFF;
+  $secondary-color: #1A202C;
+  $tertiary-color: #F7F7F7;
+  $dark-bg: #1D2430;
+  $body-text: 18px;
+  $section-title: 30px;
+  $project-title: 24px;
 
 </style>
