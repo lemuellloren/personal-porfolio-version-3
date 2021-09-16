55df472f8258decb80b7c331038e0afa381f05d6
lemuellloren <lempakz@yahoo.com>
2021-09-16 21:20:30 +0800

    set up intro contents and tech tools

--- components/Intro.vue
+++ components/Intro.vue
@@ -1,23 +1,34 @@
 <template>
   <div class="intro-container">
-   <div class="container">
- <div class="row">
-     <div class="col-lg-7">
-                <div class="intro">
-      <h1 class="intro__heading">
-        Hello, Amigo! 👋
-      </h1>
-      <h3 class="intro__subheading">
-          I’m Lemuel a <span class="intro__subheading--bold">Software Developer</span>
-      </h3>
-      <p class="intro__body">
-          I like to craft solid and scalable frontend products with great user experiences.
-      </p>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-7">
+          <div class="intro">
+            <h1 class="intro__heading">
+              Hello, Amigo! 👋
+            </h1>
+            <h3 class="intro__subheading">
+              I’m Lemuel a <span class="intro__subheading--bold">Software Developer</span>
+            </h3>
+            <p class="intro__body">
+              I like to craft solid and scalable frontend products with great user experiences.
+            </p>
+            <div class="intro__btn-group pt-3">
+                <a href="#" class="btn primary-button mr-3">Contact Me</a>
+                <a href="#" class="btn secondary-button mr-3">View Projects</a>
+            </div>
+            <!-- Technology and Tools  -->
+            <div class="techstack">
+              <div class="section-title">
+                <p class="section-title__text">
+                  Technologies &amp; Tools
+                </p>
+              </div>
+            </div>
+            <!-- Technology and Tools  -->
+          </div>
+        </div>
+      </div>
     </div>
-     </div>
- </div>
-   </div>
   </div>
 </template>
-
-
--- pages/index.vue
+++ pages/index.vue
@@ -8,10 +8,11 @@
 
 <script>
   export default {}
+
 </script>
 
 <style lang="scss">
-  //* Variables 
+  //* VARIABLES
   $primary-color: #7D5FFF;
   $secondary-color: #1A202C;
   $white: #F7F7F7;
@@ -24,8 +25,11 @@
   $font: 'Poppins',
   sans-serif;
   $transitions: all ease .4s;
+  $border: 1px solid $primary-color;
+  $body-line-height: 1.5em;
+  $heading-line-height: 1em;
 
-  //* Mixins 
+  //* MIXINS
   // menu 
   @mixin menu-link {
     font-size: 18px;
@@ -50,30 +54,104 @@
 
   // section default style
   @mixin section-default {
-    background-color:  $secondary-color;
-     padding-top: 80px;
-  padding-bottom: 80px;
+    background-color: $secondary-color;
+    padding-top: 80px;
+    padding-bottom: 80px;
   }
 
   // heading 
   @mixin heading {
     color: $white;
     font-weight: 500;
-    line-height: 1em;
+    line-height: $heading-line-height;
   }
+
   // body text 
   @mixin body-text {
     font-size: 18px;
-    line-height: 1.5em;
+    line-height: $body-line-height;
     color: $white;
     font-weight: normal;
+    transition: $transitions;
+
+    &:hover {
+      border-radius: $border-radius;
+    }
   }
-  //* Global Class 
+
+  // primary button 
+  @mixin primary-btn {
+    color: $white;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 500;
+    border-radius: $border-radius;
+    transition: $transitions;
+    background-color: $primary-color;
+
+    &:hover {
+      color: $secondary-color;
+    }
+
+  }
+
+  // secondary button 
+  @mixin secondary-btn {
+    color: $primary-color;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 500;
+    background: unset;
+    border-radius: $border-radius;
+
+    &:hover {
+      color: $primary-color;
+      border: 1px solid $primary-color;
+      border-radius: $border-radius;
+
+    }
+  }
+
+  //* GLOBAL CLASS 
+  // icons 
   .my-icons {
     color: $white;
     font-size: 30px;
   }
 
+  // buttons 
+  .primary-button {
+    @include primary-btn;
+  }
+
+  .secondary-button {
+    @include secondary-btn;
+  }
+
+  // section title 
+  .section-title {
+    &__text {
+      color: $white;
+      font-weight: 500;
+      font-size: 30px;
+      line-height: $heading-line-height;
+      position: relative;
+      margin-bottom: 30px;
+
+      &:after {
+        content: '';
+        height: 10px;
+        width: 70px;
+        position: absolute;
+        background-color: $primary-color;
+        left: 0;
+        bottom: -15px;
+      }
+    }
+  }
+
+
+  // * GLOBAL STYLES 
   //* Header
   .the-header {
     min-height: 120px;
@@ -115,23 +193,33 @@
     @include menu-link;
   }
 
-//* Intro Section 
-.intro-container {
-  @include section-default;
-  .intro {
-    &__heading {
-     @include heading
-    }
-    &__subheading {
-      @include heading
-    }
-     &__subheading--bold {
-       color: $primary-color;
-       font-weight: 600;
+  //* Intro Section 
+  .intro-container {
+    @include section-default;
+
+    .intro {
+      &__heading {
+        @include heading
       }
+
+      &__subheading {
+        @include heading
+      }
+
+      &__subheading--bold {
+        color: $primary-color;
+        font-weight: 600;
+      }
+
       &__body {
         @include body-text;
       }
+    }
   }
-}
+
+  //* Technology Tools 
+  .techstack {
+    padding-top: 100px;
+  }
+
 </style>
